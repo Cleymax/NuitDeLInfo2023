0f13b5222d87eac0673d9b6c13ca807c596d7f15
osteriux <pixave73@gmail.com>
2023-12-07 23:48:15 +0100

    next fonctionel

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@ const main = async () => {
   const picture_div = document.querySelector("#picture");
 
   const question_div = document.querySelector("#question");
-  const option_div = document.querySelectorAll("#opt");
+  const option_div = document.querySelector("#opt");
   const options_div = document.querySelectorAll(".opts");
   const next_button = document.querySelector("#next");
 
@@ -17,6 +17,25 @@ const main = async () => {
   // variables context
   let context = 0;
   let current_opt = undefined;
+
+  // init
+  const setContext = (context) => {
+    title_div.innerText = data[context].titre;
+    context_div.innerText = data[context].text;
+    if(data[context].image){
+        picture_div.src = data[context].image
+    }
+    option_div.innerHTML = '';
+    data[context].options.forEach(opt => {
+        let op = document.createElement("li");
+        op.classList.add("opts");
+        op.innerText = opt.texte;
+        op.id = opt.idSuiv
+        option_div.appendChild(op);
+    });
+  }
+  setContext(context);
+  
 
   // add events
   for (const i of options_div) {
@@ -38,19 +57,7 @@ const main = async () => {
     // on hover, if opt choosed, cursor
     if(current_opt){
       context = current_opt.idSuiv;
-      title_div.innerText = data[context].titre;
-      context_div.innerText = data[context].text;
-      if(data[context].image){
-          picture_div.src = data[context].image
-      }
-      option_div.innerHTML = '';
-      data[context].options.forEach(opt => {
-          let op = document.createElement("li");
-          op.classList.add("opts");
-          op.innerText = opt.text;
-          op.id = opt.idSuiv
-          option_div.appendChild(op);
-      });
+      setContext(context)
     }
   });
 };
